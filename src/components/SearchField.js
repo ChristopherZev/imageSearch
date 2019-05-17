b61865e088deaec06d43c922425b8a1f06dd65f6
ChristopherZev <christopher_zev@icloud.com>
2019-05-17 10:52:41 -0400

    Created the onInputChange method and added the onChange prop/eventhandler to check for changes in user input. Passed in the event as a parameter to test the inherent event.target.value property to see how it tracks input changes

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 class SearchField extends React.Component{
+	/*
+	The onInputchange method is made to handle or be called 
+	any time someone changes the input.   
+	*/	 
+	onInputChange(event){
+		console.log(event.target.value);
+	}
+
 	render(){	
 		return(
 			<div className="ui segment">
 				<form className="ui form">
 					<div className="field">
 						<label>Search Images</label>
-						<input type="text" />
+						{/*
+						onChange is a new prop that references onInputChange
+						via the this.onInputChange callback function, however 
+						we do not write it as this.onInputChange() because
+						the () would cause it to run every time the component
+						rerenders, instead we are passing a reference to the 
+						***input element*** so the input element can call the 
+						func some time in the future.
+
+						Others examples of eventhandlers are onClick,onSubmit
+						*/}
+						<input type="text" onChange={this.onInputChange} />
 					</div>
 				</form>
 			</div>
