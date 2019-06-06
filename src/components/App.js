90e070e829b71bbae42e273fa919d6d6a7f92d61
ChristopherZev <christopher_zev@icloud.com>
2019-06-06 14:52:25 -0400

    Tested unsplash api via axios.get

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,18 @@ class App extends React.Component{
 	component
 	*/
 	onSearchSubmit(searchTerm){
-		console.log(searchTerm);
+		/*
+		Axios.get uses the Unsplash Api to return a photo based
+		on the "searchTerm" entered 
+		*/
+		axios.get('https://api.unsplash.com/search/photos',{
+			params: {
+				query: searchTerm
+			},
+			headers: {
+				Authorization: 'Client-ID 67f8a7b922bccd5adade12bf5acf5dd9092f1e9797244ac13a4a3bc1c2461ef1'
+			}
+		});
 	}
 
 	render() {
