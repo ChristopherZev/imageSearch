0939a60b3e00e465416fcb0b0d0b5631edd1eedb
Christopher Zevallos <855329@cognizant.com>
2020-02-19 16:00:23 -0500

    Update gitignore, removed extra comments from code

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,26 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
-//import axios from 'axios';
-//Best practice list import for 3rd party/dependencies before our own files
 
 import SearchField from './SearchField';
 import ListOfImages from './ListOfImages';
 
-//Test Git
 
 class App extends React.Component{
 
 	state = {pics: [] };
-	/*
-	created onSearchSubmit to pass user input from the searchfield
-	component
-	*/
 	onSearchSubmit = async (searchTerm) => {
-		/*
-		Axios.get uses the Unsplash Api to return a photo based
-		on the "searchTerm" entered 
-		*/
+		
 		const response = await unsplash.get('https://api.unsplash.com/search/photos',{
 			params: {query: searchTerm}
 		});
-		//console.log(this)
+	
 		this.setState({ pics: response.data.results});
 	}
 
 	render() {
 		return(
 			<div className="ui container" style={{marginTop: '10px'}}>
-				{/*
-				 this line called onSearchSubmit
-				*/}
+	
 				<SearchField onSubmit={this.onSearchSubmit} />
 				Found: {this.state.pics.length} pictures.
 					<br/>
--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,39 +1,11 @@
 import React from 'react';
 
 class SearchField extends React.Component{
-	/*
-	The onInputchange method is made to handle or be called 
-	any time someone changes the input. Which is uncontrolled.
 
-	It is named specifically on Input Change because it targets
-	any change made to the input tag where the func is called
-	via the onChange prop
-	
-	onInputChange(event){
-		console.log(event.target.value);
-	}
-	*/
-
-	/*
-	Controlled vs Uncontrolled components-------------------
-	Controlled is the act of storing data in React comp vs
-	HTML elements i.e. storing and using state to get the current
-	value of the input field vs leaving the HTML to determine the
-	value.
-	*/
 	state = { searchTerm: '' };
 
-	/*
-	onFormSubmit was changed to an arrow func so that the this
-	keyword gets automatically binded to value of the instance
-	of the SearchField 
-	*/
 	onFormSubmit = event => {
 		event.preventDefault();
-		/*
-		This line allows us to pass in the props from here to the 
-		App component
-		*/ 
 		this.props.onSubmit(this.state.searchTerm);
 	}
 	
@@ -43,28 +15,7 @@ class SearchField extends React.Component{
 				<form onSubmit={this.onFormSubmit}className="ui form">
 					<div className="field">
 						<label>Search Images</label>
-						{/*
-						onChange is a new prop that references onInputChange
-						via the this.onInputChange callback function, however 
-						we do not write it as this.onInputChange() because
-						the () would cause it to run every time the component
-						rerenders, instead we are passing a reference to the 
-						***input element*** so the input element can call the 
-						func some time in the future.
-
-						Others examples of eventhandlers are onClick,onSubmit
-						*/}
 						<input type="text" 
-						/*
-						onChange can have the abbreviated version using arrow
-						functions seen below as well. With this version you
-						do not need to define the onInputChange handler above
-						
-						onChange={(e) => console.log(e.target.value)} />
-						
-						an uncontrolled version would also be 
-						onChange={this.onInputChange} 
-						*/
 						value={this.state.searchTerm}
 						onChange={e => this.setState({ searchTerm: e.target.value })} />
 
