2ca9fd693ffa5a97ca3788b2364bd81bc265b7c0
Christopher Zevallos <855329@cognizant.com>
2020-02-19 20:09:41 -0500

    Updated prop.pics.map to have a key prop and an alt prop

--- src/components/ListOfImages.js
+++ src/components/ListOfImages.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ListOfImages = (props) => {
 	
-	const pics = props.pics.map((pic) =>{
-		return <img src={pic.urls.regular} alt="missing image"/>
+	const pics = props.pics.map(({description, id, urls}) =>{
+		return <img key={id} src={urls.regular} alt={description}/>
 	});
 
 	return(
